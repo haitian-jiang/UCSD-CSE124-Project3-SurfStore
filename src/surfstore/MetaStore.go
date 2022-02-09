cd6b47cd01f8bcbbc199e9778a6050357d4e4b81
hiltonjiang <jht2001120@outlook.com>
2022-02-09 20:58:21 +0800

    finish metastore

--- src/surfstore/MetaStore.go
+++ src/surfstore/MetaStore.go
@@ -1,5 +1,9 @@
 package surfstore
 
+import (
+	"fmt"
+)
+
 type MetaStore struct {
 	FileMetaMap map[string]FileMetaData
 	// Add additional data structure(s) to maintain BlockStore addresses
@@ -7,20 +11,42 @@ type MetaStore struct {
 }
 
 func (m *MetaStore) GetFileInfoMap(_ignore *bool, serverFileInfoMap *map[string]FileMetaData) error {
-	panic("todo")
+	*serverFileInfoMap = m.FileMetaMap
+	return nil
 }
 
 func (m *MetaStore) UpdateFile(fileMetaData *FileMetaData, latestVersion *int) (err error) {
-	panic("todo")
+	filename := fileMetaData.Filename
+	currFileMetaData, ok := m.FileMetaMap[filename]
+
+	// new file
+	if !ok {
+		// version number is 1 in this case
+		m.FileMetaMap[filename] = *fileMetaData
+		*latestVersion = fileMetaData.Version
+		return nil
+	}
+
+	// previously existed file
+	if fileMetaData.Version != currFileMetaData.Version+1 {
+		*latestVersion = currFileMetaData.Version
+		return fmt.Errorf("Invalid version: current version on server is %d", currFileMetaData.Version)
+	} else {
+		*latestVersion = fileMetaData.Version
+		m.FileMetaMap[filename] = *fileMetaData
+		return nil
+	}
+
 }
 
 func (m *MetaStore) GetBlockStoreMap(blockHashesIn []string, blockStoreMap *map[string][]string) error {
-	panic("todo")
+	//panic("todo")
+	*blockStoreMap = map[string][]string{m.BlockStoreAddr[0]: blockHashesIn}
+	return nil
 }
 
 var _ MetaStoreInterface = new(MetaStore)
 
 func NewMetaStore(blockStoreList []string) MetaStore {
-	//panic("todo")
 	return MetaStore{map[string]FileMetaData{}, blockStoreList}
 }
